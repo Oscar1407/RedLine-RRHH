5ff38a027a7dfb1c17fb6493aa81154df0238cf2
Oscar1407 <oscarcorrales2011@gmail.com>
2020-11-10 04:05:12 -0600

    Diseño pantalla Gestion de Cursos

---
+++ Presentacion/FrmGestionCursos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class FrmGestionCursos : Form
+    {
+
+        FrmAdministradorCapacitaciones administradorCapacitaciones;
+
+        public FrmGestionCursos()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGestionCursos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.administradorCapacitaciones = new FrmAdministradorCapacitaciones();
+            this.administradorCapacitaciones.Show();
+            this.Close();
+            this.Dispose();
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+    }
+}
