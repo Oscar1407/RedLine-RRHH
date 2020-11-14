93c7e2e945993439dd61a1592aeb0755e9706757
Oscar1407 <oscarcorrales2011@gmail.com>
2020-11-13 19:18:42 -0600

    Corrección Objeto

--- LogicaNegocio/Colaborador.cs
+++ LogicaNegocio/Colaborador.cs
@@ -20,7 +20,13 @@ namespace LogicaNegocio
 
         public string correo { set; get; }
 
+        public string correoSecundario { set; get; }
+
         public string telefono { set; get; }
+
+        public string telefonoSecundario { set; get; }
+
+        public string puestoTrabajo { get; set; }
 
     }
 }
